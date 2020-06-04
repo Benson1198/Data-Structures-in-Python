fd13561651406fb17707bf956c13dd52ff6d84f0
Benson P Xavier <34964177+Benson1198@users.noreply.github.com>
2020-06-04 16:32:39 +0530

    DLL Node

--- Doubly Linked List.py
+++ Doubly Linked List.py
@@ -3,5 +3,31 @@ class Node:
         self.data = data
         self.next = next
         self.prev = prev
+
+    def set_data(self,data):
+        self.data = data
     
+    def get_data(self):
+        return self.data
+
+    def set_next(self,next):
+        self.next = next
+
+    def get_next(self):
+        return self.next
+
+    def has_next(self):
+        return self.next != None
+
+    def set_prev(self,prev):
+        self.prev = prev
+
+    def get_prev(self):
+        return self.prev
+
+    def has_prev(self):
+        return self.prev != None
+
+    def __str__(self):
+        return "Node [Data = {}]".format(self.data)  
     
