20e877f552a6695376d6441bf1c4fb603bd4bba4
Benson P Xavier <34964177+Benson1198@users.noreply.github.com>
2020-06-04 18:09:45 +0530

    DLL Insertion 1

--- Doubly Linked List.py
+++ Doubly Linked List.py
@@ -30,4 +30,17 @@ class Node:
 
     def __str__(self):
         return "Node [Data = {}]".format(self.data)  
+
     
+    # Insert Node at Beginning od Doubly Linked List
+
+    def insert_at_beginning(self,data):
+        new_node = Node(data,None,None)
+
+        if self.head == None:
+            self.head = new_node
+        else:
+            new_node.set_prev(None)
+            new_node.set_next(self.head)
+            self.head.set_prev(new_node)
+            self.head = new_node
